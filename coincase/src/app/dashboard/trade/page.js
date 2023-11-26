49a6eb148e527de72761b8a76c6a8e035dbdb98d
Legitzx <Luciano.kho18@gmail.com>
2023-11-26 14:49:21 -0800

    Implement tablist for trade page

--- coincase/src/app/dashboard/trade/page.js
+++ coincase/src/app/dashboard/trade/page.js
@@ -2,6 +2,31 @@
 
 "use client";
 
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
 export default function trade() {
-  return <div>Trade</div>;
+  return (
+    <>
+      <Tabs isFitted variant='enclosed'>
+        <TabList mb='1em'>
+          <Tab>Buy</Tab>
+          <Tab>Sell</Tab>
+          <Tab>Send</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
+  );
+
+  
 }
