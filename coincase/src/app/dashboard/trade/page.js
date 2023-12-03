3cd3f67270faa365b6eee356b708ab2ce631b7e6
Legitzx <Luciano.kho18@gmail.com>
2023-12-02 21:22:26 -0800

    Trade UI Progress

--- coincase/src/app/dashboard/trade/page.js
+++ coincase/src/app/dashboard/trade/page.js
@@ -4,9 +4,35 @@
 
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from '@chakra-ui/react'
+
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from '@chakra-ui/react'
+
+import { Select } from '@chakra-ui/react'
+
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
 export default function trade() {
   return (
     <>
+      <Stat>
+        <StatLabel>Cash Balance</StatLabel>
+        <StatNumber>$0.00</StatNumber>
+      </Stat>
+
       <Tabs isFitted variant='enclosed'>
         <TabList mb='1em'>
           <Tab>Buy</Tab>
@@ -15,7 +41,22 @@ export default function trade() {
         </TabList>
         <TabPanels>
           <TabPanel>
-            <p>one!</p>
+            {/* BUY PANEL */}
+            <NumberInput defaultValue={0} min={0} max={10000}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+
+            <Select placeholder='Select option'>
+              <option value='option1'>Option 1</option>
+              <option value='option2'>Option 2</option>
+              <option value='option3'>Option 3</option>
+            </Select>
+            
+            <Button colorScheme='blue'>Buy CryptoName</Button>
           </TabPanel>
           <TabPanel>
             <p>two!</p>
